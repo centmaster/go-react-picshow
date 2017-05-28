fae1c58e58997b4b4a266593c2f23eeab6025bf0
centmaster <861676131@qq.com>
2017-05-28 11:31:04 +0800

    pic yes

--- picserver.go
+++ picserver.go
@@ -3,26 +3,36 @@ package main
 import (
 	"fmt"
 	"html/template"
+	//"io"
 	"net/http"
-	"regexp"
-	//"time"
-	"strings"
 	"os"
+	//"path"
+	"regexp"
+	"time"
+	"strings"
 )
 
 var mux map[string]func(http.ResponseWriter, *http.Request)
 
 type Myhandler struct{}
-
+type home struct {
+	Title string
+}
 
 const (
 	Template_Dir = "./view/"
 )
 
 func main() {
-	//http.HandleFunc("/", hello)
-    	http.HandleFunc("/api",index)
-    	http.ListenAndServe(":1037", nil)
+	server := http.Server{
+		Addr:        ":1037",
+		Handler:     &Myhandler{},
+		ReadTimeout: 10 * time.Second,
+	}
+	mux = make(map[string]func(http.ResponseWriter, *http.Request))
+	mux["/"] = index
+	mux["/showpic"] = showpic
+	server.ListenAndServe()
 }
 
 func (*Myhandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -33,15 +43,21 @@ func (*Myhandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if ok, _ := regexp.MatchString("/css/", r.URL.String()); ok {
 		http.StripPrefix("/css/", http.FileServer(http.Dir("./css/"))).ServeHTTP(w, r)
 	} else {
-		http.StripPrefix("/", http.FileServer(http.Dir("./upload/"))).ServeHTTP(w, r)
+		http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))).ServeHTTP(w, r)
 	}
 
 }
 
 
 func index(w http.ResponseWriter, r *http.Request) {
-	 r.ParseForm()
-    	fmt.Println("path",r.URL.Path)
+	title := home{Title: "首页"}
+	t, _ := template.ParseFiles(Template_Dir + "index.html")
+	t.Execute(w, title)
+}
+
+
+func showpic(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("path",r.URL.Path)
     	for k, v := range r.Form {
                 fmt.Println("key:", k)
                 fmt.Println("val:", strings.Join(v, ""))
@@ -51,5 +67,7 @@ func index(w http.ResponseWriter, r *http.Request) {
             }
         t, _ := template.ParseFiles(Template_Dir+"show.html")
         t.Execute(w,nil)
+
 }
+
 
