40e09a68102a3c1b05bda3e599dc53756f97f3d3
centmaster <861676131@qq.com>
2017-05-28 13:33:28 +0800

    complete the func

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var fs = require('fs');
 
 // viewed at http://localhost:8080
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/view/show.html'));
 });
-app.get('/api', function(req, res) {
-   console.log('chen');
-})
+app.post('/api', function (req,res) {
+    if(!isNaN(req.query.delete)){
+        var item=req.query.delete;
+        fs.unlink('./static/'+item+'.jpg',function (err) {
+            if(err) {
+                console.log('err');
+            }else{
+                console.log('success');
+            }
+            return
+        })
+    }
+});
 app.use('/static/',express.static(path.join(__dirname, './static')));
 app.use('/css/',express.static(path.join(__dirname,'./css')));
 
 app.listen(8080);
+
